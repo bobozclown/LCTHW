6c8de8c91659e9676d8a94bdf6242a9861c0001f
Dane L <Daner90@gmail.com>
2015-04-02 17:23:03 -0500

    exercise 5 done

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*this here is a comment dagnabbit*/
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	//this is also a comment!
+	printf("You ar %d miles away.\n", distance);
+
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,10 @@
+CFLAGS=-Wall -g
+SOURCES= ex1.c ex3.c ex4.c ex5.c
+OBJECTS=$(SOURCES:.c=)
+
+
+all:
+	make $(OBJECTS)
+clean:
+	rm -f $(OBJECTS)
+
