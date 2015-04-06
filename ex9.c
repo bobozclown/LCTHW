ac1a8e616f85440c016c7accbc9ad259e208f7c7
Dane L <Daner90@gmail.com>
2015-04-06 00:21:44 -0500

    partially done with 9

---
+++ ex9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char*argv[])
+{
+	int numbers[4] = {0};
+	char name[4] = {'a'};
+
+	// first, print them out raw
+	printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+
+	//add more soon
+}
