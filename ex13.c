fd1d4b136b5b2a1c1ec525af3235a149047548fd
Dane L <Daner90@gmail.com>
2015-04-10 14:14:32 -0500

    ex13 complete. added a commit script to make 4 commands into 1  Please enter the commit message for your changes. Lines starting

--- ex13.c
+++ ex13.c
@@ -18,5 +18,31 @@ int main(int argc, char *argv[])
 			break;
 		case'e':
 		case'E':
-			printf("%d: 'E'\n", i}}
-}
+			printf("%d: 'E'\n", i);
+			break;
+		case'i':
+		case'I':
+			printf("%d: 'I'\n", i);
+			break;
+		case'o':
+		case'O':
+			printf("%d: 'O'\n", i);
+			break;
+		case'u':
+		case'U':
+			printf("%d: 'U'\n", i);
+			break;
+		case'y':
+		case'Y':
+			if(i>2) {
+				//it's only sometimes Y
+				printf("%d: 'Y'\n",i);
+				}
+			break;
+		default:
+			printf("%d: %c is not a vowel\n",i,letter);
+		}//end switch
+	}//end for
+
+	return 0;
+}//end main
