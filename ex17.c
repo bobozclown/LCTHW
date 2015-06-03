29ed0970bcbf81f833ca366d504d7da8a504bc55
bobo <bobo@noemail.com>
2015-06-03 10:49:09 -0500

    ex17 compiles  Please enter the commit message for your changes. Lines starting

--- ex17.c
+++ ex17.c
@@ -21,7 +21,7 @@ struct Database {
 struct Connection {
 	FILE *file;
 	struct Database *db;
-}
+};
 
 void die(const char *message)
 {
@@ -53,12 +53,12 @@ struct Connection *Database_open(const char *filename, char mode)
 	if(!conn) die("connection null, memory error");
 
 	//create database struct within connection struct
-	con->db = malloc(sizeof(struct Database));
+	conn->db = malloc(sizeof(struct Database));
 	if(!conn->db) die("db null, memory error");
 
 	//if mode=c open file
 	if(mode == 'c') {
-		con->file = fopen(filename, "w");
+		conn->file = fopen(filename, "w");
 	} else {
 		//??? r+ w
 		conn->file = fopen(filename, "r+");
@@ -76,8 +76,8 @@ struct Connection *Database_open(const char *filename, char mode)
 void Database_close(struct Connection *conn)
 {
 	if(conn) {
-		if(con->file) fclose(conn->file);
-		if(con->db) free(conn->db);
+		if(conn->file) fclose(conn->file);
+		if(conn->db) free(conn->db);
 		free(conn);
 	}
 }
@@ -85,7 +85,7 @@ void Database_close(struct Connection *conn)
 void Database_write(struct Connection *conn)
 {
 	rewind(conn->file);
-	int rc = fwrite(conn->db,sizeof(struct Database), 1, con->file);
+	int rc = fwrite(conn->db,sizeof(struct Database), 1, conn->file);
 	if(rc !=1) die ("Failed to write database.");
 
 	rc = fflush(conn->file);
@@ -111,3 +111,87 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
 
 	addr->set = 1;
 	//WARNING: bug, read the how to break it and fix this
+	char *res = strncpy(addr->name, name, MAX_DATA);
+	//demonstrate the strncpy bug
+	if(!res) die("Email copy failed");
+}
+
+void Database_get(struct Connection *conn, int id)
+{
+	struct Address *addr = &conn->db->rows[id];
+
+	if(addr->set) {
+		Address_print(addr);
+	} else {
+		die("ID is not set");
+	}
+}
+
+void Database_delete(struct Connection *conn, int id)
+{
+	struct Address addr = {.id = id, .set = 0};
+	conn->db->rows[id] = addr;
+}
+
+void Database_list(struct Connection *conn)
+{
+	int i = 0;
+	struct Database *db =conn->db;
+
+	for(i = 0; i < MAX_ROWS; i++) {
+		struct Address *cur = &db->rows[i];
+
+		if(cur->set) {
+			Address_print(cur);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]");
+
+	char *filename = argv[1];
+	char action = argv[2][0];
+	struct Connection *conn = Database_open(filename, action);
+	int id = 0;
+
+	if(argc > 3) id = atoi(argv[3]);
+	if(id >= MAX_ROWS) die("There's not that many records");
+
+	switch(action) {
+		case 'c':
+			Database_create(conn);
+			Database_write(conn);
+			break;
+		case 'g':
+			if(argc != 4) die("Need an id to get");
+
+			Database_get(conn, id);
+			break;
+
+		case 's':
+			if(argc != 6) die("Need id, name, email to set");
+
+			Database_set(conn, id, argv[4], argv[5]);
+			Database_write(conn);
+			break;
+
+		case 'd':
+			if(argc != 4) die("Need id to delete");
+			Database_delete(conn, id);
+			Database_write(conn);
+			break;
+
+		case 'l':
+			Database_list(conn);
+			break;
+
+		default:
+			die("Invalid action, only: c=create, g=get, s=set, d=del, l=list");
+	}
+
+	Database_close(conn);
+
+	return 0;
+}
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CFLAGS=-Wall -g
-SOURCES= ex1.c ex3.c ex4.c ex5.c ex6.c ex7.c ex8.c ex9.c ex10.c ex11.c ex12.c ex13.c ex14.c ex15.c ex16.c
+SOURCES= ex1.c ex3.c ex4.c ex5.c ex6.c ex7.c ex8.c ex9.c ex10.c ex11.c ex12.c ex13.c ex14.c ex15.c ex16.c ex17.c
 OBJECTS=$(SOURCES:.c=)
 
 
